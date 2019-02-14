0d8db673a2ea93d6de8a864960d51d0603595f07
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2019-02-14 21:41:26 -0200

    Add match_words on analyze.py

--- analyze.py
+++ analyze.py
@@ -1,4 +1,4 @@
-from utils import (
+from carpet import (
     csv_to_list,
     evaluate_job_file,
     get_files_in_directory,
@@ -14,15 +14,17 @@ if __name__ == '__main__':
     job_files = get_files_in_directory(JOBS_DIRECTORY)
 
     for job_file in job_files:
-        poor_level = evaluate_job_file(job_file, metrics)
+        poor_level, match_words = evaluate_job_file(job_file, metrics)
         results.append({
             'job_file': job_file,
             'poor_level': poor_level,
+            'match_words': match_words,
         })
 
     results_by_poor_level = order_by_key(results, 'poor_level')
     for result in results_by_poor_level:
-        print('Poor level: {} - Job file: {}'.format(
+        print('Poor level: {} - Job file: {} - Words: {}'.format(
             result['poor_level'],
             result['job_file'],
+            result['match_words'],
         ))
