224115451ce8ea9c427e74d9efb4c476b4986de2
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2019-02-14 21:25:58 -0200

    Iterate over pages of pyjobs

--- download_pyjobs.py
+++ download_pyjobs.py
@@ -2,15 +2,16 @@ import os
 
 from utils import get_pyjob_codes, get_pyjob_content
 
-
 JOBS_DIRECTORY = 'jobs/pyjobs'
 
-
 if __name__ == '__main__':
-    pyjob_codes = get_pyjob_codes()
 
-    pyjob_url = 'http://www.pyjobs.com.br/job/{}/'
+    pyjob_codes = []
+    for page in range(1, 7):
+        pyjob_codes += get_pyjob_codes(page=page)
+
     for pyjob_code in pyjob_codes:
+        print('Downloading pyjob {}'.format(pyjob_code))
         content = get_pyjob_content(pyjob_code)
 
         filename = 'job{}.txt'.format(pyjob_code)
